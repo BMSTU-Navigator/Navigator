73d775f506d5e3cbd6e14f9e34f62e6e71e23a47
alexdark <evtuhov.aleksander@gmail.com>
2017-03-10 21:23:51 +0300

    код рисования

--- Nvg/WayBuilder/test.py
+++ Nvg/WayBuilder/test.py
@@ -1,6 +1,11 @@
-from Navigator.WayBuilder.Clases import GraphConnection,Graph
-cl=GraphConnection(1,1,1,1,1,1)
+from PIL import Image, ImageDraw
+import sys
+im = Image.open("cat.jpg")
+im.show()
+draw = ImageDraw.Draw(im)
+draw.line((0, 0) + im.size, fill=128)
+draw.line((0, im.size[1], im.size[0], 0), fill=128)
+del draw
 
-print(cl.picture_path)
-cl=Graph
-print(len(cl.connections))
+# write to stdout
+im.save('catout.jpg')
