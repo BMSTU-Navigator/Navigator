6050526ec012dd45876d180d9411c6e1128ceb4c
alexdark <evtuhov.aleksander@gmail.com>
2017-03-17 21:32:54 +0300

    обертка над бот мастером

--- Nvg/Bot/TelegramBot/Bot.py
+++ Nvg/Bot/TelegramBot/Bot.py
@@ -1,2 +1,10 @@
+from Nvg.old_code import telebot
+
 class Bot:
     dialog_id=-1;
+    telebot =None
+    def __init__(self,telebot,dialog_id):
+        self.telebot=telebot
+        self.dialog_id=dialog_id
+    def get_answer(self,input_string):
+        self.telebot.send_message(self.dialog_id, input_string+'  answer of bot '+str(self.dialog_id))
--- Nvg/Bot/TelegramBot/BotMaster.py
+++ Nvg/Bot/TelegramBot/BotMaster.py
@@ -4,11 +4,12 @@
 
 from Nvg.Bot.TelegramBot import config
 from Nvg.old_code import telebot
-
+from Nvg.Bot.TelegramBot.Bot import *
 
 bot = telebot.TeleBot(config.token)
 
-
+id_list=[]
+bots={}
 
 
 
@@ -17,12 +18,22 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    if(message.text!='Привет'):
-        bot.send_message(message.chat.id,'и тебе привет')
-    else:
-        bot.send_message(message.chat.id, 'start')
-        bot.send_photo(message.chat.id,open('fl2.jpeg', 'rb'))
-        bot.send_message(message.chat.id, 'stop')
+    if message.chat.id not in id_list:
+        id_list.append(message.chat.id)
+        tmp_bot=Bot(bot,message.chat.id)
+        bots[message.chat.id]=tmp_bot
+        tmp_bot=None
+
+    bots[message.chat.id].get_answer(message.text)
+
+
+
+    #if(message.text!='Привет'):
+    #    bot.send_message(message.chat.id,'и тебе привет')
+    #else:
+    #    bot.send_message(message.chat.id, 'start')
+    #    bot.send_photo(message.chat.id,open('fl2.jpeg', 'rb'))
+    #    bot.send_message(message.chat.id, 'stop')
 
 
 
