c658f51481be99670e86dd092c21bfdc0ab837cd
alexdark <evtuhov.aleksander@gmail.com>
2017-03-17 14:05:01 +0300

    праки дейкстры

--- Nvg/WayBuilder/WayBuilderClass.py
+++ Nvg/WayBuilder/WayBuilderClass.py
@@ -2,6 +2,7 @@ class WayBuilderClass:
     graph = None
     paths = None
     dijkstra_graph = None
+    dijkstra_connectons=None
     max_id = -1
 
     def init_pre_count(self):
@@ -15,26 +16,78 @@ class WayBuilderClass:
             self.dijkstra_graph[conection.point1.id][conection.point2.id]=conection.weight
 
     def dijkstra(self, start,stop):
-        valid = [True] * self.max_id
-        weight = [1000000] * self.max_id
-        weight[start] = 0
-        for i in range(self.max_id):
-            self.paths[i] = []
-            min_weight = 1000001
-            ID_min_weight = -1
-            for i in range(self.max_id):
-                if valid[i] and weight[i] < min_weight:
-                    min_weight = weight[i]
-                    ID_min_weight = i
+        self.dijkstra_connectons = []
+        for i in range(0, len(self.dijkstra_graph)):
+            self.dijkstra_connectons.append([])
+            for j in range(0, len(self.dijkstra_graph[i])):
+                if (self.dijkstra_graph[i][j] < 10000):
+                    self.dijkstra_connectons[i].append(j)
 
-            for i in range(self.max_id):
-                if weight[ID_min_weight] + self.dijkstra_graph[ID_min_weight][i] < weight[i]:
-                    weight[i] = weight[ID_min_weight] + self.dijkstra_graph[ID_min_weight][i]
-            valid[ID_min_weight] = False
-        if weight[stop]==10000:raise Exception('no path to point')
-        return weight
+        print(self.dijkstra_connectons)
+
+        start = 5
+        n = 10
+        INF = 10 ** 10
+
+        dist = [INF] * n
+        dist[start] = 0
+        prev = [None] * n
+        used = [False] * n
+        min_dist = 0
+        min_vertex = start
+        while min_dist < INF:
+            i = min_vertex
+            used[i] = True
+            for j in w[i]:
+                if dist[i] + g[i][j] < dist[j]:
+                    dist[j] = dist[i] + g[i][j]
+                    prev[j] = i
+            min_dist = INF
+            for i in range(n):
+                if not used[i] and dist[i] < min_dist:
+                    min_dist = dist[i]
+                    min_vertex = i
+
+        print('wdwdwdwdwd')
+        print(dist)
+        print('eddd')
+        j = 7
+        path = []
+        while j is not None:
+            path.append(j)
+            j = prev[j]
+        path = path[::-1]
+        print(path)
+        sum = 0
+        for i in range(0, len(path) - 1):
+            sum += g[path[i]][path[i + 1]]
+            print(sum)
 
 
+
+
+       #valid = [True] * self.max_id
+       #weight = [1000000] * self.max_id
+       #weight[start] = 0
+       #for i in range(self.max_id):
+       #    self.paths[i] = []
+       #    min_weight = 1000001
+       #    ID_min_weight = -1
+       #    for i in range(self.max_id):
+       #        if valid[i] and weight[i] < min_weight:
+       #            min_weight = weight[i]
+       #            ID_min_weight = i
+
+       #    for i in range(self.max_id):
+       #        if weight[ID_min_weight] + self.dijkstra_graph[ID_min_weight][i] < weight[i]:
+       #            weight[i] = weight[ID_min_weight] + self.dijkstra_graph[ID_min_weight][i]
+       #    valid[ID_min_weight] = False
+       #if weight[stop]==10000:raise Exception('no path to point')
+       #return weight
+
+    def request_path(self,start,stop):
+        print(self.dijkstra(start,stop))
+        print(self.paths)
 
     def psi_init(self):
         self.dijkstra_graph = []
--- Nvg/WayBuilder/wb_tester.py
+++ Nvg/WayBuilder/wb_tester.py
@@ -2,4 +2,4 @@ from Nvg.WayBuilder.WayBuilderClass import *
 
 nv = WayBuilderClass()
 nv.psi_init()
-print(nv.dijkstra(1))
+print(nv.request_path(1,5))
